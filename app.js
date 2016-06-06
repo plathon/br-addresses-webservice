5a9b60424146e93ad960b0349ea68fe6b3f796cb
Renan Plathon <rplathon@gmail.com>
2016-06-06 17:48:20 -0300

    cities by state

--- app.js
+++ app.js
@@ -20,7 +20,17 @@ var knex = require('knex')({
 var bookshelf = require('bookshelf')(knex);
 
 var States = bookshelf.Model.extend({
-  tableName: 'cepbr_estado'
+  tableName: 'cepbr_estado',
+  cities: function () {
+    return this.hasMany(Cities);
+  }
+});
+
+var Cities = bookshelf.Model.extend({
+  tableName: 'cepbr_cidade',
+  state: function() {
+    return this.belongsTo(States, 'uf');
+  }
 });
 
 restify.CORS.ALLOW_HEADERS.push('authorization');
@@ -33,10 +43,20 @@ server.use(restify.bodyParser());
 server.post('states', function (req, res, next) {
   States.fetchAll().then(function (states) {
     res.send(states.toJSON());
-    next();
+    return next();
   }).catch(function (err) {
     return next(err)
   })
+});
+
+server.post('citiesByState', function (req, res, next) {
+  var params = req.params;
+  Cities.where({ uf: params.uf }).fetch({ withRelated: ['cepbr_estado'] }).then(function (cities) {
+    res.send(JSON.stringify( cities.related('cepbr_estado') ))
+    return next();
+  }).then(function (err) {
+    return next(err)
+  });
 });
 
 //start server
